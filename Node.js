b96f2c563c9b02de1ce2a47dbe92f2d674dca96e
Pulkit Jindal <105127469+Code-Pulkit@users.noreply.github.com>
2022-05-27 18:47:13 +0530

    U

---
+++ Node.js
@@ -0,0 +1,10 @@
+Node.js is a virtual machine that uses JavaScript as its scripting language and runs Chrome’s V8 JavaScript engine.
+Npm or Yarn is the package manager for NodeJs.
+
+Node Advantages
+  1. Chrome V8 - Runs on C++
+  2. Javascript on Both Frontend and Backend
+
+The main advantage of using promise is you get an object to decide the action that needs to be taken after the async task completes. 
+This gives more manageable code and avoids callback hell.
+
